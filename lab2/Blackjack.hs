2568be659ec704da87b8a11f74d229917e5c035a
David Göransson <david.goransson90@gmail.com>
2015-11-09 15:57:35 +0100

    Fix value function to work with multiple Aces

--- lab2/Blackjack.hs
+++ lab2/Blackjack.hs
@@ -10,14 +10,28 @@ size hand2
 = 2
 -}
 
+exH :: Hand
+exH = (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
+exH1 :: Hand
+exH1 = (Add (Card Ace Hearts) (Add (Card Jack Spades) Empty))
+exH2 :: Hand
+exH2 = (Add (Card Ace Hearts) (Add (Card Ace Spades) Empty))
+exH3 :: Hand
+exH3 = (Add (Card King Hearts) (Add (Card Jack Spades) Empty))
+exH4 :: Hand
+exH4 = (Add (Card Ace Hearts) (Add (Card Ace Spades) (Add (Card Ace Diamonds) Empty)))
+
 empty :: Hand
 empty = Empty
 
 -- The value of a hand.
 
 value :: Hand -> Integer
-value Empty = 0
-value (Add card hand) = valueCard card + value hand
+value hand 
+  | value' hand <= 21 = value' hand
+  | otherwise         = value' hand - (numberOfAces hand * 10)
+      where value' Empty = 0
+            value' (Add card hand') = valueCard card + value' hand'
 
 -- The value of a card.
 
